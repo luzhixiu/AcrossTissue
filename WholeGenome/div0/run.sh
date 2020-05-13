fcef57220898070b33be0ab92681899b32b44430
luzhixiu1996 <luzhixiu1996@gmail.com>
2020-05-13 16:09:41 -0400

    added some new runs and original folders

---
+++ WholeGenome/div0/run.sh
@@ -0,0 +1,2 @@
+
+Rscript updateROC.R -i ./ -o ./ -n 8 -a 20 -t 10 > Crei.Rout 2>&1 | tee crei.txt &
---
+++ WholeGenome/div4/run.sh
@@ -0,0 +1,2 @@
+
+Rscript updateROC.R -i ./ -o ./ -n 8 -a 20 -t 10 -d 4 > Crei.Rout 2>&1 | tee crei.txt &
