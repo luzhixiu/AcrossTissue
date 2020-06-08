24b05e1ce09cebddd0ee9edd25230ae37a826635
lu <luzhixiu1996@gmail.com>
2020-06-08 12:54:50 -0400

    improved visualization for codon selection comparisons

--- ScatterCSV.py
+++ ScatterCSV.py
@@ -6,7 +6,7 @@ import numpy as np
 import scipy.stats as ss
 import validator
 headers=[]
-inputFile="/home/lu/AcrossTissue/ComparisonLSG/selectionParameterBetweenLS.csv"
+inputFile="/home/lu/AcrossTissue/ComparisonLSG/selectionParameterBetweenLS_WithoutReferenceCodon.csv"
 
 
 #format: first line should be the header, each column is a list
@@ -19,7 +19,6 @@ def parseInputFile(fname):
     headers=lines[0].rstrip().split(",")
     for line in lines[1:]:
         splitList=line.rstrip().split(",")
-        
         csvMatrix.append(splitList)
     return csvMatrix
 
@@ -40,6 +39,7 @@ matrix=np.transpose(np.asarray(csvMatrix))
 AANames=matrix[0]    
 codonNames=matrix[1]
 matrix=matrix[2:]
+headers=headers[2:]
 matrix=convertMatirxFromStringToFloat(matrix)
 
 def median(lst):
@@ -67,13 +67,26 @@ def getMeanListFromMatrix(matrix,transpose=False):
     return meanList
 
 
+#return lst1/lst2, must ensure the length is the same
+def divideListByList(lst1,lst2):
+    resultList=[]
+    for i in range(len(lst1)):
+        if lst2[i]==0:
+            resultList.append(0)
+            continue
+        result=lst1[i]/lst2[i]
+        resultList.append(result)
+    return resultList
+
+        
+
 
 
 
 
 
 #works, but the proportion is not clear in visualization
-def plotMatrixInScatter(matrix):
+def plotMatrixStackedBar(matrix):
     fig=plt.figure()
     plotGroupLS=[]
     meanList=getMeanListFromMatrix(matrix,transpose=True)
@@ -81,12 +94,13 @@ def plotMatrixInScatter(matrix):
     corelation=validator.testCorelation(medianList,meanList,"pearson" )
     if corelation<0.99:
         print "Warning! Mean and Median has a corelation less than 0.99, choice of mean of median might make a difference"
-    plotMean=plt.bar(range(1,len(medianList)+1),medianList)
-    plotGroupLS.append(plotMean)
+#    plotMean=plt.bar(range(1,len(meanList)+1),meanList)
+#    plotGroupLS.append(plotMean)
     
     for ls in matrix:
-        plotGroup=plt.bar(range(1,len(ls)+1),ls)
-        plotGroupLS.append(ls)
+        proportionLS=divideListByList(ls,meanList)
+        plotGroup=plt.bar(range(1,len(proportionLS)+1),proportionLS)
+        plotGroupLS.append(plotGroup)
     
     global codonNames
     labelList=[]
@@ -97,10 +111,21 @@ def plotMatrixInScatter(matrix):
             newS+="\n"
         labelList.append(newS)
     plt.xticks(range(1,len(medianList)+1),labelList,rotation=-5, fontsize=5)
+    legendGroup=[]
+    for i in range(len(plotGroupLS)):
+        legendGroup.append(plotGroupLS[i][0])
+    plt.legend(legendGroup,headers,fancybox=True,fontsize=5)
+    plt.ylabel("Selection Estimate Proportion")
+
     plt.show()
     fig.savefig("test.pdf",bbox_inches='tight')
 
-plotMatrixInScatter(matrix)
+plotMatrixStackedBar(matrix)
+
+
+
+    
+    
 
 
 
