91fecbe0f5445619d8d4201e6876209b4d93fff3
lu <luzhixiu1996@gmail.com>
2020-12-11 04:12:40 -0500

    added the small fixes for run_all

--- ROC_Runs/Run_6LS/runAllLS.sh
+++ ROC_Runs/Run_6LS/runAllLS.sh
@@ -1,14 +1,16 @@
 
 cp ./updateROC.R adult/ 
 cp ./updateROC.R dauer/
-cp ./updateROC.R L3L4/
-cp ./updateROC.R larvae/
+cp ./updateROC.R L1/
+cp ./updateROC.R L2/
+cp ./updateROC.R L3/
 cp ./updateROC.R emb/
 
 cp ./run.sh adult/
 cp ./run.sh dauer/
-cp ./run.sh L3L4/
-cp ./run.sh larvae/
+cp ./run.sh L1/
+cp ./run.sh L2/
+cp ./run.sh L3/
 cp ./run.sh emb/
 
 
@@ -17,9 +19,11 @@ cd "/data1/compbio/zlu21/AcrossTissue/RunResults/adult"
 ./run.sh &
 cd "/data1/compbio/zlu21/AcrossTissue/RunResults/dauer"
 ./run.sh &
-cd  "/data1/compbio/zlu21/AcrossTissue/RunResults/L3L4"
+cd  "/data1/compbio/zlu21/AcrossTissue/RunResults/L1"
 ./run.sh &
-cd "/data1/compbio/zlu21/AcrossTissue/RunResults/larvae"
+cd  "/data1/compbio/zlu21/AcrossTissue/RunResults/L2"
+./run.sh &
+cd  "/data1/compbio/zlu21/AcrossTissue/RunResults/L3"
 ./run.sh &
 cd "/data1/compbio/zlu21/AcrossTissue/RunResults/emb"
 ./run.sh &
