9047d19558ea5f889e90f9c4aabfc6c09567d5b2
luzhixiu1996 <zlu21@helix.eecs.utk.edu>
2021-04-28 13:32:04 -0400

    runs seem correct, waiting for some to finish

---
+++ ROC_Runs/Run_L2L3_Combined/hist_deltaM_not_fixed/Top_300_From_Each_Group/runAllLS.sh
@@ -0,0 +1,29 @@
+
+cp ./updateROC.R adult/ 
+cp ./updateROC.R dauer/
+cp ./updateROC.R L1/
+cp ./updateROC.R L2L3/
+cp ./updateROC.R emb/
+
+cp ./run.sh adult/
+cp ./run.sh dauer/
+cp ./run.sh L1/
+cp ./run.sh L2L3/
+cp ./run.sh emb/
+
+
+curDir="/data1/compbio/zlu21/AcrossTissue/ROC_Runs/Run_L2L3_Combined/Top_300_From_Each_Group"
+
+cd "${curDir}/adult"
+./run.sh &
+cd "${curDir}/dauer"
+./run.sh &
+cd  "${curDir}/L1"
+./run.sh &
+cd  "${curDir}/L2L3"
+./run.sh &
+cd "${curDir}/emb"
+./run.sh &
+
+
+
