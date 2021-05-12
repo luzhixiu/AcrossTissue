1153640de515c58201ad012350baddecb5b0aa0c
Michael Gilchrist <mikeg@utk.edu>
2021-05-12 18:21:40 -0400

    updateROC.R fixes. Now can use restart files, removed redundant code.

--- ROC_Runs/Run_L2L3_Combined/Fold_Diff_4/updateROC.R
+++ ROC_Runs/Run_L2L3_Combined/Fold_Diff_4/updateROC.R
@@ -1,6 +1,7 @@
 ## Description:
-## WARNING: There are mulitple copies of this file.
-## WARNING: Master version is in 
+## WARNING: There are multiple copies of this file.
+## WARNING: Master version is in AcrossTissue/ROC_Runs/Run_L2L3_Combined/Fold_Diff_4
+## WARNING: May need to setwd("./adult") if running from its master location
 ## Code orignally form Alex Cope, modified for work on C elegans
 ## More info here!
 ## File called from script run.sh
@@ -17,33 +18,31 @@ library(argparse)
 print("Clearing local object definitions prior to using script arguments")
 rm(list=ls())
 
+print(paste0("Working directory: ", getwd()))
 
-## Global parameters
+## Initial parameters
+## Could be overwritten by parameters passed when calling script
 
-## Create list indicating which sets of parameters to estimate 
-## Values could be "Selection", "Mutation", or both of them.
-
-max.runs = 2; # number of model fit runs 
-with.phi <- FALSE
-mixDef <- "allUnique"
-proportion.to.keep <- c(0.5, 0.5) ## note function uses percent.to.keep, but it's really proportion
-adaptive.proportion <- c(1, 0.5) # proportion of run that uses the adaptive MCMC
-initial.phi <- "random" # either "random", a function name, or files 
 initial.run.number <- 1; # uses restart files for run > 1
+max.runs <- 2 # number of model fit runs
+with.phi <- FALSE
+initialPhi <- "random" ## work in progress
+mixDef <- "allUnique"
+proportion.to.keep <- rep(0.5, max.runs) ## note function uses percent.to.keep, but it's really proportion
+adaptive.proportion <- c(1.0, rep(0.5, max.runs-1)) # proportion of run that uses the adaptive MCMC initial.phi <- "random" # either "random", a function name, or files 
 run.convergence.tests <- FALSE
+## Create list indicating which sets of parameters to estimate
+## Values could be "Selection", "Mutation", or both of them.
+cspToEstimate = c("Selection")
 
 ## Values to use for mutation
 ## File name within repository
-cspToEstimate = c("Selection")
 file.name = "RunResults/Crei_Mutation_NoRef.csv"
 userName = Sys.getenv("LOGNAME")
 file.mutation.values <- switch(userName,
-                               "mike" = paste0("/home/mikeg/Repositiories/AcrossTissue/", file.name),
-                               "zlu21" = paste0("/data1/compbio/zlu21/AcrossTissue", file.name)
+                               "mikeg" = paste0("/home/mikeg/Repositories/AcrossTissue/", file.name),
+                               "zlu21" = paste0("/data1/compbio/zlu21/AcrossTissue", file.name),
                                )
-
-
-
 
 ## Process arguments passed from script file
 
@@ -53,15 +52,15 @@ print("Processing arguments passed from script")
 parser <- ArgumentParser()
 parser$add_argument("-i","--input",type="character",default="./")
 parser$add_argument("-o","--output",type="character",default="./")
-parser$add_argument("-d","--div",type="integer",default=0)
-parser$add_argument("-s","--samp",type="integer",default=5000)
+parser$add_argument("-d","--div",type="integer",default=6)
+parser$add_argument("-s","--samp",type="integer",default=1000)
 parser$add_argument("-a","--adapt",type="integer",default=100)
 parser$add_argument("-t","--thin",type="integer",default=20)
 parser$add_argument("-n","--threads",type="integer",default=1)
 
 args <- parser$parse_args()
 initial.divergence.steps <- args$div
-input <- args$input
+fasta.folders <- args$input
 directory <- args$output
 thinning <- args$thin
 adaptiveWidth <- args$adapt
@@ -70,7 +69,6 @@ num_threads <- args$threads
 
 
 ## Local Functions
-
 print("Defining local functions.")
 createParameterOutput <- function(parameter,numMixtures,samples,mixture.labels,samples.percent.keep=1,relative.to.optimal.codon=F,report.original.ref=T)
 {
@@ -91,10 +89,10 @@ createTracePlots <- function(trace, model,genome,numMixtures,samples,mixture.lab
   }
 }
 
+
 ## Process arguments
 print("Processing arguments")
 
-fasta.folders <- input #, "../data/cds/sampled/",  "../data/cds/sampled/", "../data/cds/filtered/")
 fasta.files <- list.files(path=fasta.folders,pattern="*.fasta",full.names = F)
 print(fasta.files)
 mixture.labels <- unlist(strsplit(fasta.files,split=".fasta"))
@@ -111,7 +109,7 @@ mixture.indexes <- 1:numMixtures; ## This should be defined based on previous ar
 
 
 ## Loop over mixtures
-
+## 2021-05-12 Not tested for multiple mixtures
 for(mixture.index in mixture.indexes){
   genome <- initializeGenomeObject(file=fasta.paths[mixture.index],match.expression.by.id = FALSE,append = FALSE)
   mixture.sizes[mixture.index] <- length(genome)
@@ -172,15 +170,15 @@ while((!done) && (run.number <= max.runs))
 {
 
   ## Set up directories
-  dir_name <- paste0(directory,"/run_",run.number)
-  old_dir_name <- paste0(directory,"/run_",run.number-1) ## TODO: Test to make sure file exists 
+  dir_name <- paste0(directory,"run_",run.number)
+  old_dir_name <- paste0(directory,"run_",run.number-1) ## TODO: Test to make sure file exists 
 
-  dir.create(directory)
-  dir.create(dir_name)
-  dir.create(paste(dir_name,"Graphs",sep="/"))
-  dir.create(paste(dir_name,"Restart_files",sep="/"))
-  dir.create(paste(dir_name,"Parameter_est",sep="/"))
-  dir.create(paste(dir_name,"R_objects",sep="/"))
+  dir.create(directory, showWarnings = FALSE)
+  dir.create(dir_name, showWarnings = FALSE)
+  dir.create(paste(dir_name,"Graphs",sep="/"), showWarnings = FALSE)
+  dir.create(paste(dir_name,"Restart_files",sep="/"), showWarnings = FALSE)
+  dir.create(paste(dir_name,"Parameter_est",sep="/"), showWarnings = FALSE)
+  dir.create(paste(dir_name,"R_objects",sep="/"), showWarnings = FALSE)
 
   if(run.number==1){
     ## initial run, not using restart files
@@ -192,18 +190,24 @@ while((!done) && (run.number <= max.runs))
     div <-  0
   }
 
+  # set length of adaptive part of MCMC
+  adaptive.samples <- samples*thinning*adaptive.proportion[run.number]
+  
   ## Fix mutation based on values in file
   ## May need to only evaluate for run.number = 1
   if(!("Mutation" %in% cspToEstimate) ) parameter$initMutationCategories(file.mutation.values,1,TRUE)
 
   ## Initialize MCMC object
-  mcmc <- initializeMCMCObject(samples=samples,
-                               thinning=thinning,
-                               adaptive.width=adaptiveWidth,
-                               est.expression=TRUE,
-                               est.csp=TRUE,
-                               est.hyper=TRUE,
+  mcmc <- initializeMCMCObject(samples = samples,
+                               thinning = thinning,
+                               adaptive.width = adaptiveWidth,
+                               est.expression = TRUE,
+                               est.csp = TRUE,
+                               est.hyper = TRUE,
                                est.mix = FALSE)
+
+  ## Set up adaptive component
+  mcmc$setStepsToAdapt(adaptive.samples)
 
   ## Get model object
   model <- initializeModelObject(parameter, "ROC", with.phi)
@@ -215,8 +219,10 @@ while((!done) && (run.number <= max.runs))
                      
   #run mcmc on genome with parameter using model
   sys.runtime<-system.time(
-    runMCMC(mcmc, genome, model,
-            num_threads,
+    runMCMC(mcmc,
+            genome = genome,
+            model = model,
+            ncores = num_threads,
             divergence.iteration = div)
   )
 
@@ -294,7 +300,7 @@ while((!done) && (run.number <= max.runs))
     ## Run convergence tests
     for (i in 1:numMixtures)
     {
-      param.diag<-convergence.test(trace,samples=samples*proportion.to.keep,thin = thinning,what="Selection",mixture=i,frac1=0.1)
+      param.diag<-convergence.test(trace,samples=samples*proportion.to.keep[run.number],thin = thinning,what="Selection",mixture=i,frac1=0.1)
       z.scores <- param.diag$z[which(abs(param.diag$z) > 1.96)]
       if (length(z.scores) > 0)
       {
@@ -306,7 +312,7 @@ while((!done) && (run.number <= max.runs))
 
     for (i in 1:numMixtures)
     {
-      param.diag<-convergence.test(trace,samples=samples*proportion.to.keep,thin = thinning,what="Mutation",mixture=i,frac1=0.1)
+      param.diag<-convergence.test(trace,samples=samples*proportion.to.keep[run.number],thin = thinning,what="Mutation",mixture=i,frac1=0.1)
       z.scores <- param.diag$z[which(abs(param.diag$z) > 1.96)]
       if (length(z.scores) > 0)
       {
@@ -317,7 +323,7 @@ while((!done) && (run.number <= max.runs))
 
 
 
-    diag <- convergence.test(mcmc,samples = samples*proportion.to.keep,thin=thinning,frac1=0.2)
+    diag <- convergence.test(mcmc,samples = samples*proportion.to.keep[run.number],thin=thinning,frac1=0.2)
     z<-abs(diag$z)
     done <- (z > 1.96) && param.conv
   }
